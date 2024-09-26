44a5c32ab98937e5c4ba8347bdca33ed360f38bd
Ingrails-m2 <ingrails-m2@Ingrails-m2s-Mac-mini.local>
2024-09-26 13:31:13 +0545

    Added python lsp and treesitter for yaml and python

--- lua/vimwithme/plugins/treesitter.lua
+++ lua/vimwithme/plugins/treesitter.lua
@@ -5,7 +5,7 @@ require("lazy").setup({ {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "go", "typescript" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "go", "typescript", "python", "yaml"},
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
